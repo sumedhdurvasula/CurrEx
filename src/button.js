8c5456a4152385a087ebdb9a842fa188838fe587
sumedhdurvasula <144955207+sumedhdurvasula@users.noreply.github.com>
2024-02-13 21:10:41 -0500

    Update button.js

--- src/button.js
+++ src/button.js
@@ -10,18 +10,20 @@ const CalculateButton = ({ selectedCountry, onResult }) => {
     const enterLoading = async () => {
       setLoading(true);
       
-      try {
-        console.log(selectedCountry)
-        const response = await axios.post('http://127.0.0.1:5000/hello', {
-          data: { selectedCountry }
-        });
-        console.log("Response data:", response.data)
-        onResult(response.data);
-      } catch (error) {
-          console.error("Error fetching data:", error);
-      } finally {
-          setLoading(false);
-      }
+      // try {
+      //   console.log(selectedCountry)
+      //   const response = await axios.post('http://127.0.0.1:5000/hello', {
+      //     data: { selectedCountry }
+      //   });
+      //   console.log("Response data:", response.data)
+      //   onResult(response.data);
+      // } catch (error) {
+      //     console.error("Error fetching data:", error);
+      // } finally {
+      //     setLoading(false);
+      // }
+      onResult("Placeholder for News Articles due to API Limit.");
+      setLoading(false);
   };
 
     return (
